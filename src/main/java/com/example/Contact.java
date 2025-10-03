d902889393065334068c80fcd72958bfe40db5c5
dmitrijgajdukov <dima-gaidukov@mail.ru>
2025-10-03 13:30:44 +0300

    ДЗ

---
+++ src/main/java/com/example/Contact.java
@@ -0,0 +1,71 @@
+package com.example;
+
+import java.util.Objects;
+
+public class Contact {
+    private String name;
+    private String phone;
+    private String email;
+    private String group;
+    public Contact(String name, String phone, String email, String group) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.group = group;
+    }
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(name, contact.name) && Objects.equals(email, contact.email) && Objects.equals(phone, contact.phone) && Objects.equals(group, contact.group);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phone, email, group);
+    }
+
+    @Override
+    public String toString() {
+        return "Контактные данные: " +
+                "Имя: " + name + '\'' +
+                ", Телефрн: " + phone + '\'' +
+                ", Почта: " + email + '\'' +
+                ", Группа: " + group + '\'';
+    }
+}
